c7603addfd576a5228f4309c8114acd18c6c451c
Andrew Chon <Andrewyhchon@gmail.com>
2024-01-11 00:33:04 -0800

    recreated reset and the population soft cap at least works

--- main.js
+++ main.js
@@ -2,14 +2,32 @@ const gameEngine = new GameEngine();
 
 const ASSET_MANAGER = new AssetManager();
 
+// function reset() {
+// 	if (gameEngine.board) {
+// 		gameEngine.board.reset();
+// 	} else {
+// 		gameEngine.entities = [];
+// 		new Automata(gameEngine);
+// 	}
+// };
+
 function reset() {
-	if (gameEngine.board) {
-		gameEngine.board.reset();
-	} else {
-		gameEngine.entities = [];
-		new Automata(gameEngine);
-	}
-};
+    // Load parameters or call a method to load them
+    loadParameters();
+
+    // Destroy and rebuild the world
+    destroyWorld();
+    buildWorld();
+}
+
+function destroy() {
+	gameEngine.entities = [];
+}
+
+function buildWorld() {
+	gameEngine.addEntity(new World());
+}
+
 
 ASSET_MANAGER.downloadAll(() => {
 	const canvas = document.getElementById("gameWorld");
