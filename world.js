184792afb6e5d250ac1d815026d33eefd0bc99b3
Andrew Chon <andrewyhchon@gmail.com>
2024-01-31 12:10:37 -0800

    tried adding in a different break down approach to getting genes still causing freezing

--- world.js
+++ world.js
@@ -28,7 +28,7 @@ class World {
 
     updateData() {
         this.humanPop = 0;
-        let learningTicket = 0;
+        this.genePop = 0;
         for (let i = 0; i < PARAMETERS.worldDimension; i++) {
             for (let j = 0; j < PARAMETERS.worldDimension; j++) {
                 // let cell = this.world[i][j]; 
@@ -36,17 +36,23 @@ class World {
                 
                // add in other loop here for genes for the index of n - 2 and n-1 for social and learning tickets
                // for each agent average the tickets
-               for(let k = 0; k < this.world[i][j].population.length; k++) {
-                    learningTicket = this.world[i][j].population[k].genes[k-2]
-               }
-            }
+            //    for(let k = 0; k < this.world[i][j].population.length; k++) {
+            //         this.genePop += this.world[i][j].population[k].genes[k-2]
+            //    }
+                for (let k = 0; k < this.world[i][j].population.length; k++) {
+                    let agent = this.world[i][j].population[k];
+                    let genesLength = agent.genes.length;
+                    if (genesLength >= 2) {
+                        this.genePop += agent.genes[genesLength - 2] + agent.genes[genesLength - 1];
+                    }
+                
+                }
            
+            }
+       
         }
         this.popGraph.push(this.humanPop);
         // this.geneGraph.push(genePop)
-      
-    
-
     };
 
     update() {
