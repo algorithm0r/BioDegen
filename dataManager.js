be49da1945e347f4c1c17e152879ea3a593d2bea
Andrew Chon <andrewyhchon@gmail.com>
2024-10-24 20:39:35 -0700

    got all villages to be combined into mega lists for village social, learning and averageGenes I left the village parameter just in case we want specific village data

--- dataManager.js
+++ dataManager.js
@@ -14,10 +14,28 @@ class DataManager {
         this.villageSocial = [];
         this.villageAverageGenes = [];
 
+   
     };
 
 
     logData(village) {
+        // Parse through the data here for all villages
+     // Iterate through each village to aggregate their data
+        // Resetting village data arrays
+        this.villageLearning = [];
+        this.villageSocial = [];
+        this.villageAverageGenes = [];
+
+        // Aggregate data from each village
+        for (let i = 0; i < this.world.world.length; i++) {
+            for (let j = 0; j < this.world.world[i].length; j++) {
+                let village = this.world.world[i][j];
+                this.villageLearning.push([...village.villageLearning]);
+                this.villageSocial.push([...village.villageSocial]);
+                this.villageAverageGenes.push([...village.villageAverageGenes]);
+            }
+        }
+        // =============================================
         let data = {
             db: PARAMETERS.db,
             collection: PARAMETERS.collection,
@@ -30,10 +48,13 @@ class DataManager {
                 learningTickets: this.learningGraph,
                 socialTickets: this.socialGraph,
                 
-                //  pulls directly from the current village we have clicked and gets the data from that village
-                villageLearning: village.villageLearning,
-                villageSocial: village.villageSocial,
-                villageGeneTraits: village.villageAverageGenes
+                //  pulls directly from the current village we have clicked and gets the data from that village 
+                // instead of it just pushing one list of these tickets for one village we want all the villages and for each of these lists to be 
+                //  a list of lists for every village
+                villageLearning: this.villageLearning.flat(),
+                villageSocial: this.villageSocial.flat(),
+                villageGeneTraits: this.villageAverageGenes.flat()
+                
             }
     
          }
